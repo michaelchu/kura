ac96ee33387b532dbbca32bdfbf148f66c0d604c
Michael Chu <mchchu88@gmail.com>
2020-12-31 02:13:07 -0500

    Remove advanced table functions

--- components/Cards/DataTable/DataTable.js
+++ components/Cards/DataTable/DataTable.js
@@ -8,18 +8,18 @@ const DataTable = ({ data, title, headers }) => (
     <div class="card-header">
       <h3 class="card-title">{title}</h3>
     </div>
-    <div class="card-body border-bottom py-3">
+    {/* <div class="card-body border-bottom py-3">
       <DataTableFilter />
-    </div>
+    </div> */}
     <div class="table-responsive">
       <table class="table card-table table-vcenter text-nowrap datatable">
         <DataTableHeader headers={headers} />
         <DataTableBody data={data} headers={headers} />
       </table>
     </div>
-    <div>
+    {/* <div>
       <DataTableFooter data={data} />
-    </div>
+    </div> */}
   </div>
 );
 
