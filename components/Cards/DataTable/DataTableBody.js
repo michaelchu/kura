0eb2c29f588302cceb0fde386ea62c2468c9ed5a
Michael Chu <mchchu88@gmail.com>
2020-12-31 19:56:05 -0500

    Remove action button from row

--- components/Cards/DataTable/DataTableBody.js
+++ components/Cards/DataTable/DataTableBody.js
@@ -12,28 +12,13 @@ const DataTableBody = ({ data, headers }) => (
               </td>
             ))}
             <td class="text-end">
-              <span class="dropdown">
-                <button
-                  class="btn dropdown-toggle align-text-top"
-                  data-bs-boundary="viewport"
-                  data-bs-toggle="dropdown"
-                >
-                  Actions
-                </button>
-                <div class="dropdown-menu dropdown-menu-end">
-                  <a
-                    href="#"
-                    class="dropdown-item"
-                    data-bs-toggle="modal"
-                    data-bs-target="#modal-edit-trans"
-                  >
-                    Edit
-                  </a>
-                  <a class="dropdown-item" href="#">
-                    Delete
-                  </a>
-                </div>
-              </span>
+              <a
+                href="#"
+                data-bs-toggle="modal"
+                data-bs-target="#modal-edit-trans"
+              >
+                Edit
+              </a>
             </td>
           </tr>
         );
