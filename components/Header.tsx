508e41b1a6c6d4f184215aff9300da027ed3c481
Michael Chu <mchchu88@gmail.com>
2021-01-03 01:22:45 -0500

    Fix header imports

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,36 +1,28 @@
 import Head from "next/head";
+import React from "react";
 
 const Header = () => (
   <Head>
-    <meta charset="utf-8" />
+    <meta charSet="utf-8" />
     <meta
       name="viewport"
       content="width=device-width, initial-scale=1, viewport-fit=cover"
     />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title class="next/head">Profital</title>
+    <title className="next/head">Profital</title>
 
     <script
       type="text/javascript"
       src="js/libs/bootstrap/dist/js/bootstrap.bundle.min.js"
-    ></script>
+    />
     <script
       type="text/javascript"
       src="js/libs/jquery/dist/jquery.slim.min.js"
-    ></script>
-    <script
-      type="text/javascript"
-      src="js/libs/apexcharts/dist/apexcharts.min.js"
-    ></script>
-    <script
-      type="text/javascript"
-      src="js/libs/jqvmap/dist/jquery.vmap.min.js"
-    ></script>
-    <script
-      type="text/javascript"
-      src="js/libs/jqvmap/dist/maps/jquery.vmap.world.js"
-    ></script>
-    <script type="text/javascript" src="js/tabler.min.js"></script>
+    />
+    <script type="text/javascript" src="js/tabler.min.js" />
+    {/*<script*/}
+    {/*  type="text/javascript"*/}
+    {/*  src="js/libs/apexcharts/dist/apexcharts.min.js"*/}
+    {/*></script>*/}
   </Head>
 );
 
