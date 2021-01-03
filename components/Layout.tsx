69b9bd1f3df6bacf70fb8379b7a381b7ba7b4edd
Michael Chu <mchchu88@gmail.com>
2021-01-03 01:34:09 -0500

    Compilation fixes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,15 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import NavBar from "./Navbar/NavBar";
+import React from "react";
 
-const Layout = ({ children }) => (
+const Layout = (props) => (
   <div>
     <Header />
     <div className="page">
       <NavBar />
       <div className="content">
-        <div className="container-xl">{children}</div>
+        <div className="container-xl">{props.children}</div>
         <Footer />
       </div>
     </div>
--- components/TransactionTable/TransactionTableFooter.tsx
+++ components/TransactionTable/TransactionTableFooter.tsx
@@ -1,22 +1,24 @@
-const TransactionTableFooter = ({ data }) => (
-  <div class="card-footer d-flex align-items-center">
-    <p class="m-0 text-muted">
+import React from "react";
+
+const TransactionTableFooter = () => (
+  <div className="card-footer d-flex align-items-center">
+    <p className="m-0 text-muted">
       Showing <span>1</span> to <span>8</span> of <span>16</span> entries
     </p>
-    <ul class="pagination m-0 ms-auto">
-      <li class="page-item disabled">
-        <a class="page-link" href="#" tabindex="-1" aria-disabled="true">
+    <ul className="pagination m-0 ms-auto">
+      <li className="page-item disabled">
+        <a className="page-link" href="#" tabIndex={-1} aria-disabled="true">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="icon"
+            className="icon"
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            stroke-width="2"
+            strokeWidth="2"
             stroke="currentColor"
             fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <polyline points="15 6 9 12 15 18" />
@@ -24,45 +26,45 @@ const TransactionTableFooter = ({ data }) => (
           prev
         </a>
       </li>
-      <li class="page-item">
-        <a class="page-link" href="#">
+      <li className="page-item">
+        <a className="page-link" href="#">
           1
         </a>
       </li>
-      <li class="page-item active">
-        <a class="page-link" href="#">
+      <li className="page-item active">
+        <a className="page-link" href="#">
           2
         </a>
       </li>
-      <li class="page-item">
-        <a class="page-link" href="#">
+      <li className="page-item">
+        <a className="page-link" href="#">
           3
         </a>
       </li>
-      <li class="page-item">
-        <a class="page-link" href="#">
+      <li className="page-item">
+        <a className="page-link" href="#">
           4
         </a>
       </li>
-      <li class="page-item">
-        <a class="page-link" href="#">
+      <li className="page-item">
+        <a className="page-link" href="#">
           5
         </a>
       </li>
-      <li class="page-item">
-        <a class="page-link" href="#">
+      <li className="page-item">
+        <a className="page-link" href="#">
           next{" "}
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="icon"
+            className="icon"
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            stroke-width="2"
+            strokeWidth="2"
             stroke="currentColor"
             fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <polyline points="9 6 15 12 9 18" />
