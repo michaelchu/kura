0de8f8cf69755e56d52d42a466e0253bf2df6f74
Michael Chu <mchchu88@gmail.com>
2021-12-09 21:54:03 -0500

    Replace SVG with tabler icons

--- components/ClosedPositionTable/ClosedPositionTableHeader.tsx
+++ components/ClosedPositionTable/ClosedPositionTableHeader.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { IconSearch } from '@tabler/icons';
 
 const ClosedPositionTableHeader = ({ filter, setFilter }) => (
   <>
@@ -14,22 +15,7 @@ const ClosedPositionTableHeader = ({ filter, setFilter }) => (
             onChange={(e) => setFilter(e.target.value)}
           />
           <span className="input-icon-addon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="icon"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <circle cx="10" cy="10" r="7" />
-              <line x1="21" y1="21" x2="15" y2="15" />
-            </svg>
+              <IconSearch />
           </span>
         </div>
       </div>
--- components/TransactionTable/TransactionTableHeader.tsx
+++ components/TransactionTable/TransactionTableHeader.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import { IconSearch } from '@tabler/icons';
 import { useMutation, useQueryClient } from "react-query";
 import { GraphQLClient } from "graphql-request";
 import useModal from "../../hooks/useModal";
@@ -72,22 +72,7 @@ export default function TransactionTableHeader({
               onChange={(e) => setFilter(e.target.value)}
             />
             <span className="input-icon-addon">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="icon"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <circle cx="10" cy="10" r="7" />
-                <line x1="21" y1="21" x2="15" y2="15" />
-              </svg>
+              <IconSearch />
             </span>
           </div>
         </div>
