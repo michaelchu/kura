a72d71d0eb0b46d8d53acf8e7edef42cb2204abd
Michael Chu <mchchu88@gmail.com>
2021-12-10 00:27:15 -0500

    Add accounting format for closed positions

--- components/ClosedPositionTable/Columns.tsx
+++ components/ClosedPositionTable/Columns.tsx
@@ -24,22 +24,78 @@ export const COLUMNS = [
   {
     Header: "Original Cost",
     accessor: "original_cost",
-    Cell: ({ value }) => accounting.formatMoney(value),
+    Cell: ({ value }) => {
+      if (value < 0) {
+        return (
+          <span className={"text-success"}>
+            {accounting.formatMoney(Math.abs(value))}
+            <i> CR</i>
+          </span>
+        );
+      } else if (value > 0) {
+        return (
+          <span className={"text-danger"}>
+            {accounting.formatMoney(value)}
+            <i> DR</i>
+          </span>
+        );
+      } else {
+        accounting.formatMoney(value);
+      }
+    },
   },
   {
     Header: "Exit Cost",
     accessor: "exit_cost",
-    Cell: ({ value }) => accounting.formatMoney(value),
+    Cell: ({ value }) => {
+      if (value < 0) {
+        return (
+          <span className={"text-success"}>
+            {accounting.formatMoney(Math.abs(value))}
+            <i> CR</i>
+          </span>
+        );
+      } else if (value > 0) {
+        return (
+          <span className={"text-danger"}>
+            {accounting.formatMoney(value)}
+            <i> DR</i>
+          </span>
+        );
+      } else {
+        return accounting.formatMoney(value);
+      }
+    },
   },
   {
     Header: "Total Fees",
     accessor: "total_fees",
-    Cell: ({ value }) => accounting.formatMoney(value),
+    Cell: ({ value }) => {
+      return (
+        <span className={"text-danger"}>{accounting.formatMoney(value)}</span>
+      );
+    },
   },
   {
     Header: "Realized P/L",
     accessor: "realized_pnl",
-    Cell: ({ value }) => accounting.formatMoney(value),
+    Cell: ({ value }) => {
+      if (value < 0) {
+        return (
+          <span className={"text-success"}>
+            {accounting.formatMoney(Math.abs(value))}
+          </span>
+        );
+      } else if (value > 0) {
+        return (
+          <span className={"text-danger"}>
+            {accounting.formatMoney(value * -1)}
+          </span>
+        );
+      } else {
+        return accounting.formatMoney(value);
+      }
+    },
   },
   {
     Header: "Days in Trade",
