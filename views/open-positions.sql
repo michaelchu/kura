10aab3d6770699f0b9f0022289d5ea9bd94d3450
Michael Chu <mchchu88@gmail.com>
2021-12-13 13:57:29 -0500

    Fix open positions query

--- views/open-positions.sql
+++ views/open-positions.sql
@@ -1,26 +1,29 @@
 DROP VIEW IF EXISTS open_positions;
 CREATE OR REPLACE VIEW open_positions AS
-WITH open_positions as (
-    SELECT symbol, strategy, sum(quantity)
-    from trades
-    GROUP BY symbol, strategy
-    having sum(quantity) <> 0
+WITH open_positions AS (
+    SELECT trades.root,
+           trades.strategy,
+           trades.asset_type,
+           sum(trades.quantity) AS sum
+    FROM trades
+    GROUP BY trades.root, trades.strategy, trades.asset_type
+    HAVING sum(trades.quantity) <> 0
 )
-
-SELECT s.display as strategy,
-       trade_date,
-       root,
+SELECT s.display AS strategy,
+       t.trade_date,
+       t.root,
        t.action,
-       expiration,
-       type,
-       strike,
-       total_cost,
-       asset_type,
+       t.quantity,
+       t.expiration,
+       t.type,
+       t.strike,
+       t.total_cost,
+       t.asset_type,
        a.name,
-       account_id
-from open_positions op
-         INNER JOIN trades t on (t.symbol = op.symbol and t.strategy = op.strategy) or
-                                (t.root = op.symbol and t.strategy = op.strategy)
-         INNER JOIN accounts a on t.account_id = a.id
-INNER JOIN strategies s on op.strategy = s.name
-ORDER BY root, trade_date DESC;
+       t.account_id
+FROM open_positions op
+         JOIN trades t
+              ON (t.root = op.root AND t.strategy = op.strategy and t.asset_type = op.asset_type)
+         JOIN accounts a ON t.account_id = a.id
+         JOIN strategies s ON op.strategy = s.name
+ORDER BY t.root, t.trade_date DESC;
