640f34f7a2d0e6868e59e608ecc71c8c57011fda
Michael Chu <mchchu88@gmail.com>
2021-12-16 13:14:50 -0500

    Update project to use new kura hasura endpoint

---
+++ views/transaction_costs.sql
@@ -0,0 +1,23 @@
+DROP VIEW IF EXISTS transaction_costs;
+CREATE VIEW transaction_costs AS
+SELECT transactions.id,
+       symbol,
+       trade_date,
+       price,
+       fee,
+       CASE
+           WHEN asset_type = 'option' THEN
+               (price * quantity * 100) + fee
+           when asset_type = 'stock' THEN
+               (price * quantity) + fee
+           END   as total_cost,
+       account_id,
+       quantity,
+       asset_type,
+       action,
+       strategy,
+       s.display as strategy_name,
+       a.name    as account_name
+from transactions
+         inner join strategies s on transactions.strategy = s.name
+         inner join accounts a on transactions.account_id = a.id
