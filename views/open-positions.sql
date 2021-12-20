83d746dabee8f419d2706f2d9e950d65fd32a002
Michael Chu <mchchu88@gmail.com>
2021-12-20 13:24:25 -0500

    Fix open positions query

--- views/open-positions.sql
+++ views/open-positions.sql
@@ -1,32 +1,63 @@
 DROP VIEW IF EXISTS open_positions;
 CREATE OR REPLACE VIEW open_positions AS
-WITH open_positions AS (
-    SELECT trades.root,
-           trades.strategy,
-           trades.asset_type,
-           sum(trades.quantity) AS sum
-    FROM trades
-    GROUP BY trades.root, trades.strategy, trades.asset_type
-    HAVING sum(trades.quantity) <> 0
-)
-SELECT s.display AS strategy,
-       t.trade_date,
-       t.root,
-       t.symbol,
-       t.action,
-       t.quantity,
-       t.expiration,
-       t.type,
-       t.strike,
-       t.price,
-       t.fee,
-       t.total_cost,
-       t.asset_type,
-       a.name,
-       t.account_id
-FROM open_positions op
-         JOIN trades t
-              ON (t.root = op.root AND t.strategy = op.strategy and t.asset_type = op.asset_type)
-         JOIN accounts a ON t.account_id = a.id
-         JOIN strategies s ON op.strategy = s.name
-ORDER BY t.root, t.trade_date DESC;
+WITH grouped_trades_open as (
+    SELECT symbol,
+           action,
+           strategy,
+           root,
+           max(trade_date) as trade_date,
+           sum(quantity)   as quantity,
+           sum(total_cost) as total_cost,
+           sum(fee)        as fees,
+           expiration,
+           type,
+           strike,
+           asset_type,
+           account_id
+    from trades
+    where action = 'BTO'
+       or action = 'STO'
+    group by symbol, action, strategy, root, expiration, type, strike, asset_type, account_id
+),
+     grouped_trades_closed as (
+         SELECT symbol,
+                action,
+                strategy,
+                root,
+                max(trade_date) as trade_date,
+                sum(quantity)   as quantity,
+                sum(total_cost) as total_cost,
+                sum(fee)        as fees,
+                expiration,
+                type,
+                strike,
+                asset_type,
+                account_id
+         from trades
+         where action <> 'BTO'
+           and action <> 'STO'
+         group by symbol, action, strategy, root, expiration, type, strike, asset_type, account_id
+     ),
+     agg_trades as (
+         SELECT *
+         from grouped_trades_open
+         UNION ALL
+         SELECT *
+         from grouped_trades_closed
+     )
+
+SELECT symbol,
+       strategy,
+       root,
+       sum(quantity) as quantity,
+       expiration,
+       type,
+       strike,
+       asset_type,
+       account_id,
+       u.id          as user_id
+FROM agg_trades
+         INNER JOIN accounts a ON agg_trades.account_id = a.id
+         INNER JOIN users u on a.user_id = u.id
+group by symbol, strategy, root, expiration, type, strike, asset_type, account_id, u.id
+having sum(quantity) <> 0;
