4b840db6094b582d2f58b9176e1ca17e426f6ff3
Michael Chu <mchchu88@gmail.com>
2022-01-08 11:51:51 -0500

    Initial setup of elixir backend auth

---
+++ api/auth.tsx
@@ -0,0 +1,88 @@
+import React, { useState, useContext, createContext } from "react";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  gql,
+} from "@apollo/client";
+
+const authContext = createContext({});
+
+export function AuthProvider({ children }) {
+  const auth = useProvideAuth();
+
+  return (
+    <authContext.Provider value={auth}>
+      <ApolloProvider client={auth.createApolloClient()}>
+        {children}
+      </ApolloProvider>
+    </authContext.Provider>
+  );
+}
+
+export const useAuth = () => {
+  return useContext(authContext);
+};
+
+function useProvideAuth() {
+  const [authToken, setAuthToken] = useState(null);
+
+  const isSignedIn = () => {
+    return !!authToken;
+  };
+
+  const getAuthHeaders = () => {
+    if (!authToken) return null;
+
+    return {
+      authorization: `Bearer ${authToken}`,
+    };
+  };
+
+  const createApolloClient = () => {
+    const link = new HttpLink({
+      uri: "http://localhost:4000/api",
+      headers: getAuthHeaders(),
+    });
+
+    return new ApolloClient({
+      link,
+      cache: new InMemoryCache(),
+    });
+  };
+
+  const signIn = async ({ username, password }) => {
+    const client = createApolloClient();
+    const LoginMutation = gql`
+      mutation Login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+          token
+        }
+      }
+    `;
+
+    const result = await client.mutate({
+      mutation: LoginMutation,
+      variables: { username, password },
+    });
+
+    console.log(result);
+
+    if (result?.data?.login?.token) {
+      setAuthToken(result.data.login.token);
+    }
+  };
+
+  const signOut = () => {
+    setAuthToken(null);
+  };
+
+  return {
+    setAuthToken,
+    isSignedIn,
+    signIn,
+    signOut,
+    createApolloClient,
+  };
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import { Hydrate } from "react-query/hydration";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { AuthProvider } from "../api/auth";
 import "../styles.css";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
-      </Hydrate>
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+      </QueryClientProvider>
+    </AuthProvider>
   );
 }
