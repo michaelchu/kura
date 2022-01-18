7e1ff6d2148dbcb2aa24cbfe5b20eb9d058ec52d
Michael Chu <mchchu88@gmail.com>
2022-01-17 20:21:40 -0500

    Fix blue border on top of login/signup pages

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -28,69 +28,63 @@ export default function SignUp() {
   });
 
   return (
-    <div className="antialiased border-top-wide border-primary d-flex flex-column">
-      <div className="page page-center login-dark">
-        <div className="container-tight py-4">
-          <div className="text-center mb-4">
-            <img src="/logo-dark.png" height="36" alt="" />
-          </div>
-          <div className="card card-md">
-            <div className="card-body">
-              <h1 className="text-center mb-4">Create your free account</h1>
-              <div className="mb-3">
-                <label className="form-label">Email address</label>
+    <div className="page page-center login-dark">
+      <div className="container-tight py-4">
+        <div className="text-center mb-4">
+          <img src="/logo-dark.png" height="36" alt="" />
+        </div>
+        <div className="card card-md">
+          <div className="card-body">
+            <h1 className="text-center mb-4">Create your free account</h1>
+            <div className="mb-3">
+              <label className="form-label">Email address</label>
+              <input
+                type="email"
+                className={error ? "form-control is-invalid" : "form-control"}
+                placeholder="Enter email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Password</label>
+              <div className="input-group input-group-flat">
                 <input
-                  type="email"
+                  type="password"
                   className={error ? "form-control is-invalid" : "form-control"}
-                  placeholder="Enter email"
-                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
-              <div className="mb-3">
-                <label className="form-label">Password</label>
-                <div className="input-group input-group-flat">
-                  <input
-                    type="password"
-                    className={
-                      error ? "form-control is-invalid" : "form-control"
-                    }
-                    placeholder="Password"
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-              </div>
-              {error && (
-                <div className="text-danger mb-3">
-                  Oops! User already exist!
-                </div>
-              )}
-              <div className="mb-3">
-                <label className="form-check">
-                  <input type="checkbox" className="form-check-input" />
-                  <span className="form-check-label">
-                    I agree to the{" "}
-                    <a href="./terms-of-service.html" tabIndex={-1}>
-                      terms and policy
-                    </a>
-                    .
-                  </span>
-                </label>
-              </div>
-              <div className="form-footer">
-                <button
-                  type="submit"
-                  className="btn btn-primary w-100"
-                  disabled={loading}
-                  onClick={() => signUp()}
-                >
-                  Create new account
-                </button>
-                <div className="text-center text-muted mt-3">
-                  Already have account?{" "}
-                  <Link href="/login">
-                    <a>Sign in</a>
-                  </Link>
-                </div>
+            </div>
+            {error && (
+              <div className="text-danger mb-3">Oops! User already exist!</div>
+            )}
+            <div className="mb-3">
+              <label className="form-check">
+                <input type="checkbox" className="form-check-input" />
+                <span className="form-check-label">
+                  I agree to the{" "}
+                  <a href="./terms-of-service.html" tabIndex={-1}>
+                    terms and policy
+                  </a>
+                  .
+                </span>
+              </label>
+            </div>
+            <div className="form-footer">
+              <button
+                type="submit"
+                className="btn btn-primary w-100"
+                disabled={loading}
+                onClick={() => signUp()}
+              >
+                Create new account
+              </button>
+              <div className="text-center text-muted mt-3">
+                Already have account?{" "}
+                <Link href="/login">
+                  <a>Sign in</a>
+                </Link>
               </div>
             </div>
           </div>
