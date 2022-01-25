25f55bb56df4ebffd6d751b87b9dd324f3f31833
Michael Chu <mchchu88@gmail.com>
2022-01-24 23:05:59 -0500

    Add resetStore to login/logout process

--- components/Layouts/NavBar.tsx
+++ components/Layouts/NavBar.tsx
@@ -5,6 +5,7 @@ import useStorage from "../../hooks/useStorage";
 import NavDropdown from "./NavDropdown";
 import Nav from "./Nav";
 import Link from "next/link";
+import { ApolloConsumer } from "@apollo/client";
 
 export default function NavBar({ toggleModal }) {
   const router = useRouter();
@@ -12,50 +13,56 @@ export default function NavBar({ toggleModal }) {
   const { removeItem } = useStorage();
 
   return (
-    <header className="navbar navbar-expand-md navbar-dark sticky-top d-print-none">
-      <div className="container-xl">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbar-menu"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <h1 className="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
-          <Link href=".">
-            <a>
-              <img
-                src="/logo-dark.png"
-                width="110"
-                height="32"
-                alt="kura"
-                className="navbar-brand-image"
-              />
-            </a>
-          </Link>
-        </h1>
-        {isSignedIn && (
-          <NavDropdown
-            signOut={() => {
-              removeItem("token");
-              router.push("/login");
-            }}
-            router={router}
-            toggleModal={toggleModal}
-          />
-        )}
-        {isSignedIn && <Nav router={router} />}
-        {!isSignedIn && (
-          <div className="navbar-nav flex-row order-md-last">
-            <div className="nav-item dropdown me-3">
-              <Link href="/login">
-                <a className="nav-link px-0">Login</a>
+    <ApolloConsumer>
+      {(client) => (
+        <header className="navbar navbar-expand-md navbar-dark sticky-top d-print-none">
+          <div className="container-xl">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbar-menu"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <h1 className="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
+              <Link href=".">
+                <a>
+                  <img
+                    src="/logo-dark.png"
+                    width="110"
+                    height="32"
+                    alt="kura"
+                    className="navbar-brand-image"
+                  />
+                </a>
               </Link>
-            </div>
+            </h1>
+            {isSignedIn && (
+              <NavDropdown
+                signOut={() => {
+                  client.resetStore().then(() => {
+                    removeItem("token");
+                    router.push("/login");
+                  });
+                }}
+                router={router}
+                toggleModal={toggleModal}
+              />
+            )}
+            {isSignedIn && <Nav router={router} />}
+            {!isSignedIn && (
+              <div className="navbar-nav flex-row order-md-last">
+                <div className="nav-item dropdown me-3">
+                  <Link href="/login">
+                    <a className="nav-link px-0">Login</a>
+                  </Link>
+                </div>
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </header>
+        </header>
+      )}
+    </ApolloConsumer>
   );
 }
--- pages/login.tsx
+++ pages/login.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { gql, useMutation } from "@apollo/client";
 import useStorage from "../hooks/useStorage";
 import useSession from "../hooks/useSession";
+import { ApolloConsumer } from "@apollo/client";
 
 export default function Login() {
   const LoginMutation = gql`
@@ -41,67 +42,75 @@ export default function Login() {
   }, []);
 
   return (
-    <div className="page page-center login-dark">
-      <div className="container-tight py-4">
-        <div className="text-center mb-4">
-          <img src="/logo-dark.png" height="36" alt="" />
-        </div>
-        <div className="card card-md">
-          <div className="card-body">
-            <h1 className="text-center mb-4">Welcome Back</h1>
-            <div className="card-subtitle text-center">
-              Enter your credentials to access your account.
+    <ApolloConsumer>
+      {(client) => (
+        <div className="page page-center login-dark">
+          <div className="container-tight py-4">
+            <div className="text-center mb-4">
+              <img src="/logo-dark.png" height="36" alt="" />
             </div>
-            <div className="mb-3">
-              <label className="form-label">Email address</label>
-              <input
-                type="email"
-                className={error ? "form-control is-invalid" : "form-control"}
-                placeholder="Enter email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="mb-2">
-              <label className="form-label">
-                Password
-                <span className="form-label-description">
-                  <a href="/forgot-password">Forgot your password?</a>
-                </span>
-              </label>
-              <div className="input-group input-group-flat">
-                <input
-                  type="password"
-                  className={error ? "form-control is-invalid" : "form-control"}
-                  placeholder="Password"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-            </div>
-            {error && (
-              <div className="text-danger mb-3">
-                Oops! The email/password you entered is incorrect.
-              </div>
-            )}
-            <div className="form-footer">
-              <button
-                type="submit"
-                className="btn btn-primary w-100"
-                disabled={loading}
-                onClick={() => login()}
-              >
-                <IconLogin />
-                Sign in
-              </button>
-              <div className="text-center text-muted mt-3">
-                Don't have account yet?{" "}
-                <Link href="/signup">
-                  <a>Sign up</a>
-                </Link>
+            <div className="card card-md">
+              <div className="card-body">
+                <h1 className="text-center mb-4">Welcome Back</h1>
+                <div className="card-subtitle text-center">
+                  Enter your credentials to access your account.
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Email address</label>
+                  <input
+                    type="email"
+                    className={
+                      error ? "form-control is-invalid" : "form-control"
+                    }
+                    placeholder="Enter email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="mb-2">
+                  <label className="form-label">
+                    Password
+                    <span className="form-label-description">
+                      <a href="/forgot-password">Forgot your password?</a>
+                    </span>
+                  </label>
+                  <div className="input-group input-group-flat">
+                    <input
+                      type="password"
+                      className={
+                        error ? "form-control is-invalid" : "form-control"
+                      }
+                      placeholder="Password"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                </div>
+                {error && (
+                  <div className="text-danger mb-3">
+                    Oops! The email/password you entered is incorrect.
+                  </div>
+                )}
+                <div className="form-footer">
+                  <button
+                    type="submit"
+                    className="btn btn-primary w-100"
+                    disabled={loading}
+                    onClick={() => login().then(() => client.resetStore())}
+                  >
+                    <IconLogin />
+                    Sign in
+                  </button>
+                  <div className="text-center text-muted mt-3">
+                    Don't have account yet?{" "}
+                    <Link href="/signup">
+                      <a>Sign up</a>
+                    </Link>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </ApolloConsumer>
   );
 }
