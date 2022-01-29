6c7de6fecf0118c0119e224277d2f156643b7b48
Michael Chu <mchchu88@gmail.com>
2022-01-29 17:19:00 -0500

    Disable zoom on mobile

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,6 +20,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Rubik"
           />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+          />
         </Head>
 
         <body className="theme-light">
